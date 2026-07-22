a0426b6834a42010ccbe3b22b18578e28af14880
Anwar5533 <anwarkornipalli@gmail.com>
2026-07-22 14:33:07 +0530

    chore: remove sensitive test scripts and reports

--- apps/api/check-title.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const jobs = await prisma.job.findMany();
-  jobs.forEach(j => console.log(j.title));
-}
-main().finally(() => prisma.$disconnect());
